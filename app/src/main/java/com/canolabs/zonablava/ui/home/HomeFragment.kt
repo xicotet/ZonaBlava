b33e093f2d6f54c4a4a36971f8027ebece598fe9
CanoLabs <contact@pablocano.org>
2023-06-03 19:34:09 +0200

    Add a mapview and make status bar transparent

--- app/src/main/java/com/canolabs/zonablava/ui/home/HomeFragment.kt
+++ app/src/main/java/com/canolabs/zonablava/ui/home/HomeFragment.kt
@@ -1,22 +1,24 @@
 package com.canolabs.zonablava.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.canolabs.zonablava.R
 import com.canolabs.zonablava.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class HomeFragment : Fragment() {
+
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding!! // This property is only valid between onCreateView and onDestroyView.
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,21 +28,55 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this)[HomeViewModel::class.java]
 
+        activity?.window?.apply {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            statusBarColor = Color.TRANSPARENT
+        }
+
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // Hide the action bar
-        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        //To show normal Status Bar in other fragments
+        activity?.window?.statusBarColor =
+            ContextCompat.getColor(requireContext(), R.color.secondary_color)
         _binding = null
     }
+
+    override fun onMapReady(map: GoogleMap) {
+        map?.let {
+            googleMap = it
+            googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            googleMap.uiSettings.isZoomControlsEnabled = true
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+
 }
