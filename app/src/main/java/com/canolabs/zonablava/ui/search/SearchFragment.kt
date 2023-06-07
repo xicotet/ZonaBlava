d77b2d8cc22aba33eeecd20f144a7692e87c4729
Pablo Cano <pablocanofranch@gmail.com>
2023-06-07 22:23:43 +0200

    Add search fragment and navigation actions

---
+++ app/src/main/java/com/canolabs/zonablava/ui/search/SearchFragment.kt
@@ -0,0 +1,39 @@
+package com.canolabs.zonablava.ui.search
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.canolabs.zonablava.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var searchViewModel: SearchViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        searchViewModel =
+            ViewModelProvider(this)[SearchViewModel::class.java]
+
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/canolabs/zonablava/ui/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.canolabs.zonablava.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
